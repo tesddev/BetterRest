763f3cc277b9273d053865bc709919390b6b57b7
tesddev <tesleem.amuda@gmail.com>
2023-11-06 21:46:04 +0100

    Initial Commit

---
+++ BetterRest/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  BetterRest
+//
+//  Created by Tes on 03/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var wakeUp = Date.now
+    @State private var sleepAmount = 8.0
+    @State private var coffeeAmount = 1
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
